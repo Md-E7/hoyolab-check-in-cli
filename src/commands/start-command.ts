4ceb0acfe72ef237f2e8e8d1dbe80a01ee9c2621
Md-E7 <madeegi7@gmail.com>
2023-09-26 15:34:08 +0800

    :loud_sound: Update logger in start-command.ts

--- src/commands/start-command.ts
+++ src/commands/start-command.ts
@@ -17,25 +17,32 @@ export const startCommand = createCommand('start')
 
       if (account.honkai_impact_act_id != null && account.honkai_impact_act_id.length !== 0) {
         const honkaiImpactService = new HonkaiImpactService(account.cookie, account.honkai_impact_act_id)
-        await honkaiImpactService.start(account)
+
+        if (await honkaiImpactService.start(account)) {
+          logger.info(`Check-in otomatis pada akun ${account.name} di game Honkai Impact berhasil`)
+        }
       } else {
         logger.warn(`Check-in otomatis pada akun ${account.name} di game Honkai Impact dilewati karena honkai_impact_act_id null atau kosong`)
       }
 
       if (account.genshin_impact_act_id != null && account.genshin_impact_act_id.length !== 0) {
         const genshinImpactService = new GenshinImpactService(account.cookie, account.genshin_impact_act_id)
-        await genshinImpactService.start(account)
+
+        if (await genshinImpactService.start(account)) {
+          logger.info(`Check-in otomatis pada akun ${account.name} di game Genshin Impact berhasil`)
+        }
       } else {
         logger.warn(`Check-in otomatis pada akun ${account.name} di game Genshin Impact dilewati karena genshin_impact_act_id null atau kosong`)
       }
 
       if (account.honkai_star_rail_act_id != null && account.honkai_star_rail_act_id.length !== 0) {
         const honkaiStarRailService = new HonkaiStarRailService(account.cookie, account.honkai_star_rail_act_id)
-        await honkaiStarRailService.start(account)
+
+        if (await honkaiStarRailService.start(account)) {
+          logger.info(`Check-in otomatis pada akun ${account.name} di game Honkai Star Rail berhasil`)
+        }
       } else {
-        logger.warn(`Check-in otomatis pada akun ${account.name} di game Genshin Impact dilewati karena honkai_star_rail_act_id null atau kosong`)
+        logger.warn(`Check-in otomatis pada akun ${account.name} di game Honkai Star Rail dilewati karena honkai_star_rail_act_id null atau kosong`)
       }
     }
-
-    logger.info('Check-in otomatis dijalankan')
   })
